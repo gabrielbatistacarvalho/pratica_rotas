aebaa156e594920e1d29bec39b8a71c672e28a6b
Gabriel Batista de Carvalho <44482023+gabrielbatistacarvalho@users.noreply.github.com>
2022-09-30 17:09:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// configurção inicial
+require('dotenv').config()
+const { application } = require('express')
+const express  = require('express')
+const mongoose = require('mongoose')
+const app      = express()
+
+// forma de ler JSON / middlewares
+app.use(
+    express.urlencoded({
+        extended:true,
+    }),
+)
+
+app.use(express.json())
+
+// rodas da API
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+// rota inicial / endpoint
+app.get('/', (req, res) => {
+// mostra req
+    res.json({ message: 'oi Express!' })
+
+})
+
+// entregar uma  porta
+const DB_USER     = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rfrtxaq.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+)
+
+.then(() => {
+    console.log('Conectamos ao MongoDB')
+    app.listen(3000)
+})
+.catch((err) => console.log(err))
